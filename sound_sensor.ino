13009936449c5e15e467869602d063acaef75c01
Richard "Dylan" McGee <sleepyanorak@gmail.com>
2017-10-31 22:03:03 -0700

    Add files via upload

---
+++ sound_sensor.ino
@@ -0,0 +1,30 @@
+#define trig_pin 13
+#define echo_pin 12
+#define led 7
+
+
+void setup() {
+Serial.begin(9600);
+pinMode(trig_pin,OUTPUT);
+pinMode(echo_pin,INPUT);
+pinMode(led,OUTPUT);
+}
+
+void loop() {
+  long duration;
+  long distance;
+  digitalWrite(trig_pin,LOW);
+  delay(10);
+  digitalWrite(trig_pin,HIGH);
+  delay(10);
+  digitalWrite(trig_pin,LOW);
+  duration = pulseIn(echo_pin,HIGH);
+  distance = (duration/2) / 29.1;
+  Serial.println(distance);
+  if (distance<20){
+    digitalWrite(led,HIGH);
+  }
+  else{
+    digitalWrite(led,LOW);
+  }
+}
