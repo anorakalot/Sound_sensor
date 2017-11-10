41fa29f1957dafb390dfd1cc5489a37a097d00e2
Richard "Dylan" McGee <sleepyanorak@gmail.com>
2017-11-10 02:03:49 -0800

    Add files via upload

--- sound_sensor.ino
+++ sound_sensor.ino
@@ -4,7 +4,10 @@
 #define led_1 7
 #define led_2 6
 #define piezo 3
+//#include <cstdlib>
+//#include <ctime>
 
+//srand(time(0));
 //GLOBAL VARIABLES
 int myarray[] = {0,50,100,150,200,250,300,350,400,450,500,550,600,650,700,750,800,850,900,950,1000};
 short myarraysize = 21;
@@ -15,7 +18,8 @@ char  goback_input;
 int ascend_descend_delay = 50;
 int beats_delay = 50;
 int bell_delay = 1000;
-
+int bump_delay = 100;
+int oh_biscuits_delay = 20;
 
 
 //MAKE SURE YOU UPDATE myarraysize WHEN YOU CHANGE MYARRAY ELEMENTS SIZE
@@ -24,6 +28,7 @@ int bell_delay = 1000;
 //YOU CAN CHECK EXACT DISTANCE IN THE SERIAL MONITOR AS WELL
 
 void setup() {
+  randomSeed(analogRead(0));
   Serial.begin(9600);
   pinMode(trig_pin,OUTPUT);
   pinMode(echo_pin,INPUT);
@@ -34,11 +39,11 @@ void setup() {
   Serial.println("Welcome to the Security Program");
   Serial.println("Which alarm do you wnat?");
   Serial.println("1. Ascending 2. Descending 3. Single Low  4. Single High 5.Single Mid 6. Beats 7. Beats_2 ");
-  Serial.println("8.Bells 9.Bells_2");
+  Serial.println("8.Bells 9.Bells_2 q.Bump_Low w.Bump_Mid e.Bump_High r.Normal Alarm(Low To High) t.OhBiscuits ");
   Serial.println();
-  Serial.println("If you want to go back to change your music selection just press 9 during the program running!");
-  input = NULL;
-  while(input == NULL){ //|| input != '1' || input != '2'){
+  Serial.println("If you want to go back to change your music selection just press R during the program running!");
+  input = '?';
+  while(input == '?'){ //|| input != '1' || input != '2'){
   if(Serial.available() > 0){
     input = Serial.read();
     delay(500);
@@ -56,12 +61,12 @@ void setup() {
   //end of setup function
 }
 
+//Music functions
 void ascending(){
 for (int x = 0; x < myarraysize; ++x){
   tone(3,myarray[x]);
   delay(ascend_descend_delay);
   }
-//end of ascending function
 }
 
 void descending(){
@@ -69,7 +74,6 @@ void descending(){
   tone(3,myarray[x]);
   delay(ascend_descend_delay);
  }
-//end of descending function
 }
 
 void single_low(){
@@ -124,9 +128,66 @@ void bells_2(){
   delay(bell_delay);
 }
 
+void bump_low(){
+  tone(3,myarray[0]);
+  delay(bump_delay);
+  noTone(3);
+  delay(bump_delay);
+}
 
 
-//end of music selections
+void bump_mid(){
+  tone(3,myarray[10]);
+  delay(bump_delay);
+  noTone(3);
+  delay(bump_delay);
+}
+
+
+
+void bump_high(){
+  tone(3,myarray[20]);
+  delay(bump_delay);
+  noTone(3);
+  delay(bump_delay);
+}
+
+
+
+void normal_alarm(){
+  tone(3,myarray[0]);
+  tone(3,myarray[20]);
+}
+
+
+void oh_biscuits(){
+  tone(3,myarray[random(0,21)]);
+  delay(random(0,2000));
+  tone(3,myarray[random(0,21)]);
+  delay(random(0,2000));
+  tone(3,myarray[random(0,21)]);
+  delay(random(0,2000));
+  tone(3,myarray[random(0,21)]);
+}
+
+/*
+void super_mario(){
+  tone(3,myarray[20]);
+  delay(1000);
+  tone(3,myarray[10]);
+  delay(100);
+  tone(3,myarray[0]);
+  delay(100);
+  tone(3,myarray[5]);
+  delay(50);
+  tone(3,myarray[7]);
+  delay(50);
+  
+}
+*/
+
+
+//end of music selections8
 void nomusic(){
   noTone(3);
 }
@@ -147,13 +208,13 @@ void loop() {
   if (distance<30){
     digitalWrite(led_1,HIGH);
     digitalWrite(led_2,HIGH);
+
+    //input music choices
     if (input == '1'){
       ascending();
-      //end of choice ascending
     }
     else if(input == '2'){
       descending();
-      //end of choice descending
     }
     else if(input == '3'){
       single_low();
@@ -176,8 +237,26 @@ void loop() {
     else if (input == '9'){
       bells_2();
     }
+    else if (input == 'q'){
+      bump_low();
+    }
+    
+    else if (input == 'w'){
+      bump_mid();
+    }
 
-   
+    else if(input == 'e'){
+      bump_high();
+    }
+
+    else if(input == 'r'){
+      normal_alarm();
+    }
+    
+    else if(input == 't'){
+      oh_biscuits();
+    }
+    
     else{
       ascending();
     }
@@ -194,7 +273,7 @@ if (Serial.available()> 0){
     goback_input = Serial.read();
     delay(500);
 
-if (goback_input == '9'){
+if (goback_input == 'R'){
   setup();
   }
 }
